6a7a87b50f492f5bd12664e5faccd87c49ec305e
Pradeep Chandu <58159402+oreodigi@users.noreply.github.com>
2026-07-12 01:48:34 +0530

    Fix 3BHK planner stylesheet import

--- src/app/planner/3bhk/page.tsx
+++ src/app/planner/3bhk/page.tsx
@@ -1,3 +1,3 @@
-import '../home3d.css';
+import '../../home3d.css';
 import { HomePlanner3D } from '@/components/visualizer/HomePlanner3D';
 export default function ThreeBhkPlannerPage(){return <HomePlanner3D/>}
